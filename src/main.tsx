44327237101ac323d2e25f678e8e7e3fff2be4b0
Mohit Prajapat <mohitdevelopment2001@gmail.com>
2025-04-24 18:41:41 +0530

    Routers Added

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { AppRoutes } from "@/routes";
 import { ThemeProvider } from "@/providers/theme-providers";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <AppRoutes />
     </ThemeProvider>
   </StrictMode>
 );
