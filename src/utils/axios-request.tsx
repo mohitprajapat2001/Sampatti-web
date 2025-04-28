f4da879bded2c7a941499f95939469205082fed4
Mohit Prajapat <mohitdevelopment2001@gmail.com>
2025-04-28 18:29:14 +0530

    Update axios-request.tsx

--- src/utils/axios-request.tsx
+++ src/utils/axios-request.tsx
@@ -1,3 +1,37 @@
 /**
  * Axios Requests
  */
+// /**
+//  * Common function for axios requests
+//  * @param url string
+//  * @param method string
+//  * @param data any
+//  * @param headers any
+//  * @param config any
+//  * @param callback ()=>{}
+//  * @param errorCallback ()=>{}
+//  */
+// function axiosRequest(
+//   url: string,
+//   method: string,
+//   data: any,
+//   headers: any,
+//   config: any,
+//   callback: any,
+//   errorCallback: any
+// ) {
+//   axios({
+//     url: url,
+//     method: method,
+//     data: data,
+//     headers: headers,
+//     ...config,
+//   })
+//     .then((response) => {
+//       callback(response);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       errorCallback(error);
+//     });
+// }
